20b8978e9448aca108941b31f11b282de25b5296
Erwin Bonsma <eriban@users.sourceforge.net>
2021-02-27 10:53:54 +0100

    Make socket accessible outside container

--- backend/src/GameService.py
+++ backend/src/GameService.py
@@ -3,7 +3,7 @@ import websockets
 from service.GameServer import GameServer
 
 game_server = GameServer()
-start_server = websockets.serve(game_server.main, "127.0.0.1", 8765)
+start_server = websockets.serve(game_server.main, "", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
