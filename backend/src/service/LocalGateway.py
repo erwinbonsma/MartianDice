b59ecac775b6832392021f973c7a7fe95e210d4e
Erwin Bonsma <eriban@users.sourceforge.net>
2021-03-07 19:04:23 +0100

    Let LocalGateway pass socket ID instead of socket
    Passing socket ID as this can be stored in cache.
    This also reflects setup with AWS Gateway.

--- backend/src/service/LocalGateway.py
+++ backend/src/service/LocalGateway.py
@@ -10,17 +10,25 @@ logger = logging.getLogger('gateway')
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-class SocketCommunication:
-	async def send(self, connection, message):
-		await connection.send(message)
-
 class LocalGateway:
 	def __init__(self):
 		self.db = InMemoryDb()
-		self.comms = SocketCommunication()
+		self.comms = self
 		self.logger = logger
 
+		self.next_socket_id = 1
+		self.sockets = {}
+
+	async def send(self, socket_id, message):
+		socket = self.sockets.get(socket_id, None)
+		if socket:
+			await socket.send(message)
+
 	async def main(self, websocket, path):
+		socket_id = self.next_socket_id
+		self.sockets[socket_id] = websocket
+		self.next_socket_id += 1
+
 		try:
 			async for message in websocket:
 				self.logger.info(f"Message received: {message}")
@@ -28,15 +36,16 @@ class LocalGateway:
 				cmd = cmd_message["action"]
 
 				if cmd == "join" or cmd == "create-game":
-					cmd_handler = RegistrationHandler(self.db, self.comms, websocket)
+					cmd_handler = RegistrationHandler(self.db, self.comms, socket_id)
 				elif cmd == "start-game" or cmd == "move" or cmd == "bot-move":
-					cmd_handler = GamePlayHandler(self.db, self.comms, websocket)
+					cmd_handler = GamePlayHandler(self.db, self.comms, socket_id)
 				else:
-					cmd_handler = MetaGameHandler(self.db, self.comms, websocket)
+					cmd_handler = MetaGameHandler(self.db, self.comms, socket_id)
 
 				await cmd_handler.handle_command(cmd_message)
 		except Exception as e:
 			self.logger.info(e)
 			raise e
 		finally:
+			del self.sockets[socket_id]
 			await DisconnectionHandler(self.db, self.comms, websocket).handle_disconnect()
