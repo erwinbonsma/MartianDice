15d3074a7d8ad13a14c254952e6a19304ffd36d9
Erwin Bonsma <eriban@users.sourceforge.net>
2021-03-15 22:57:31 +0100

    More layout fixes

--- frontend/src/components/ContinueTurnCheck.js
+++ frontend/src/components/ContinueTurnCheck.js
@@ -7,11 +7,11 @@ export function ContinueTurnCheck(props) {
 	return (
 		<Container>
 			<Row>
-				<Col sm={2} />
-				<Col sm={3} ><Button id="yes" onClick={props.onAnswer} style={{width: "100%"}}>Yes</Button></Col>
-				<Col sm={2} />
-				<Col sm={3} ><Button id="no" onClick={props.onAnswer} style={{width: "100%"}}>No</Button></Col>
-				<Col sm={2} />
+				<Col sm={2} xs={0} />
+				<Col sm={3} xs={5} ><Button id="yes" onClick={props.onAnswer} style={{width: "100%"}}>Yes</Button></Col>
+				<Col sm={2} xs={2} />
+				<Col sm={3} xs={5} ><Button id="no" onClick={props.onAnswer} style={{width: "100%"}}>No</Button></Col>
+				<Col sm={2} xs={0}/>
 			</Row>
 		</Container>
 	)
--- frontend/src/components/GameSetup.js
+++ frontend/src/components/GameSetup.js
@@ -2,47 +2,44 @@ import { Separator } from './Separator';
 import Button from 'react-bootstrap/Button';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 
 export function GameSetup(props) {
 	return (
 		<div className="GameSetup">
 			<div className="ClientTable">
 				<h4 className="TableHeader">Humans</h4>
-				<Container className="TableBody">
+				<div className="TableBody">
 				{ props.clients.map(client => (
-					<Row key={client}>
-						<Col>{client}</Col>
-						<Col style={{textAlign: "right"}}>{ (client === props.host) && "Host"}</Col>
-					</Row>
+					<div key={client} style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+						<div >{client}</div>
+						<div style={{textAlign: "right"}}>{ (client === props.host) && "Host"}</div>
+					</div>
 				))}
-				</Container>
+				</div>
 				<Separator />
 			</div>
 			{ (props.bots?.length > 0 || props.isHost) && (
 				<div className="BotsTable">
 					<h4 className="TableHeader">Bots</h4>
-					<Container className="TableBody">
+					<div className="TableBody">
 					{ props.bots.map(bot => (
-						<Row key={bot}>
-							<Col>{bot}</Col>
-							<Col style={{textAlign: "right"}}>{ props.isHost && (
+						<div key={bot} style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+							<div>{bot}</div>
+							<div style={{textAlign: "right"}}>{ props.isHost && (
 								<Button className="Button" variant="secondary" size="sm" onClick={props.onRemoveBot} id={bot}>Remove</Button>
-							)}</Col>
-						</Row>
+							)}</div>
+						</div>
 					))}
 					{ props.isHost && (
-						<Row>
-							<Col><DropdownButton variant="secondary" title="Add bot" onSelect={props.onAddBot} style={{margin: "0.3em 0 0.5em"}}>
+						<div>
+							<DropdownButton variant="secondary" title="Add bot" onSelect={props.onAddBot} style={{margin: "0.3em 0 0.5em"}}>
 								<Dropdown.Item eventKey="random">Random</Dropdown.Item>
 								<Dropdown.Item eventKey="defensive">Defensive</Dropdown.Item>
 								<Dropdown.Item eventKey="smart">Smart</Dropdown.Item>
-							</DropdownButton></Col>
-						</Row>
+							</DropdownButton>
+						</div>
 					)}
-					</Container>
+					</div>
 					<Separator />
 				</div>
 			)}
