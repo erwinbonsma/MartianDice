a86112d8a2161dbba9d727d72906c8b98df16d04
Erwin Bonsma <eriban@users.sourceforge.net>
2021-03-19 14:20:34 +0100

    Show fancy game result
    Show ranked scores with bars

--- frontend/src/components/GameResult.js
+++ frontend/src/components/GameResult.js
@@ -1,3 +1,25 @@
+
 export function GameResult(props) {
-	return <h4>{props.game.winner} has won!</h4>
+	const ranked = Object.entries(props.game.scores).sort(
+		(a, b) => b[1] - a[1]
+	);
+	const maxScore = ranked[0][1];
+
+	return (<>
+		<h4>Results</h4>
+		<div style={{margin: "0 1em"}}>
+		{	ranked.map(result => {
+				const playerName = result[0];
+				const score = result[1];
+				const textClassName = (score === maxScore) ? "Winner" : "";
+				const barClassName = (score === maxScore) ? "ScoreBar-Winner" : "ScoreBar";
+				return (<div key={result[0]} style={{display: "flex", justifyContent: "space-between", margin: "0.2em" }}>
+					<div className={textClassName} style={{flexGrow: 10, flexBasis: 0, margin: "0 0.2em 0 0" }}>{playerName}</div>
+					<div className={barClassName} style={{flexGrow: score, flexBasis: 0}}></div>
+					<div className={textClassName} style={{flexGrow: 3 + (maxScore - score), flexBasis: 0, margin: "0 0 0 0.5em"}} >{score}</div>
+				</div>);
+			})
+		}
+		</div>
+	</>)
 }
