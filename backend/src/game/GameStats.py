2c5b2447a672b90acb78369250a2913559231227
Erwin Bonsma <eriban@users.sourceforge.net>
2021-05-24 20:28:09 +0200

    Add pvp matrix for num_runs = 100000

--- backend/src/game/GameStats.py
+++ backend/src/game/GameStats.py
@@ -27,3 +27,10 @@ if __name__ == '__main__':
 	players = [RandomPlayer(), AggressivePlayer(), DefensivePlayer(), op1, op2]
 	m = build_pvp_matrix(players, 1000)
 	print(m)
+
+	# Result from run with num_runs = 100000
+	# [[0.52269 0.46619 0.14833 0.07076 0.06384]
+	#  [0.57006 0.51936 0.26260 0.15804 0.15225]
+	#  [0.88807 0.78197 0.55335 0.29518 0.27899]
+	#  [0.95299 0.87960 0.79029 0.55612 0.54123]
+	#  [0.95552 0.88500 0.80395 0.57171 0.55634]]
